7d14ca325b1ace03f8abb3837790c5962c1251c3
Владимир Куклин <privet1vovik@gmail.com>
2023-03-05 14:30:45 +0300

    Выделение компонента

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,10 @@
     <h1 class="app__title">Тестовая работа M2LAB с использованием Vue3</h1>
 
     <div class="container">
-      <button 
-        class="apartment" 
-        @click="viemModalApartment"
-        >
-        {{ choiseApartment.length > 0 ? choiseApartment.join(', ') : 'Тип недвижимости'}}
-        <span 
-          @click.stop="removeChoisApartment"
-          class="remove"
-          :class="{remove_action: choiseApartment.length > 0}"
-        >
-        +
-        </span>
-        <span 
-          class="check"
-          :class="{check_action: modalApartment}"
-            >
-         </span>
-      </button>
-        <div  class="apartment__modal" :class="{active: modalApartment}">
-          <p 
-            @click="checkApartment"
-            class="apartment__modal-point"
-            v-for="typeApartmen in typeApartmens"
-            >
-            {{ typeApartmen }}
-          </p>
-        </div>
+      <RealEstateTypes 
+        :typeApartmens="typeApartmens"
+        
+      />
       <div class="room">
         <button class="room__button" type="button">Комнатность<span></span><span></span></button>
         <div class="room__modal">
@@ -41,34 +18,22 @@
 </template>
 
 <script>
+import RealEstateTypes from './components/RealEstateTypes.vue';
 import axios from 'axios';
 
 export default {
+  components: {
+    RealEstateTypes,
+  },
+
   data() {
     return {
       typeApartmens: [],
       typeRooms: [],
-      choiseApartment: [],
-      modalApartment: false,
     }
   },
-  methods: {
-    viemModalApartment() {
-      this.modalApartment = !this.modalApartment;
-    },
-    checkApartment(e) {
-      const apartment = e.target.outerText;
-      if (!this.choiseApartment.find((i) => i === apartment)) {
-        this.choiseApartment.push(e.target.outerText)
-      } else {
-        this.choiseApartment = this.choiseApartment.filter((i) => i !== apartment)
-      }
-      this.viemModalApartment()
-    },
-    removeChoisApartment() {
-      this.choiseApartment = [];
-    },
 
+  methods: {
     async fetchFilter() {
       try {
         const response = await axios.get('http://widget-server.m2lab.ru/getVillage/4985be39-cbea-4393-beef-2115868487ef');
@@ -104,75 +69,5 @@ export default {
     justify-content: space-between;
     position: relative;
   }
-
-  .apartment {
-    display: block;
-    width: 296px;
-    height: 40px;
-    padding: 12px 10px;
-    border: 1px solid #d2d2d2;
-    border-radius: 8px;
-    background-color: #fff;
-    text-rendering: optimizeSpeed;
-    display: flex;
-    align-items: center;
-    position: relative;
-  }
-
-  .check {
-    display: block;
-    width: 8px;
-    height: 8px;
-    border-bottom: 1px solid #69B574;
-    border-left: 1px solid #69B574;
-    transform: rotate(-45deg);
-    position: absolute;
-    right: 10px;
-    top: 13px;
-    transition: all .5s ease;
-  }
-
-  .check_action {
-    transform: rotate(-225deg);
-    top: 15px
-  }
-
-  .remove {
-    display: none;
-    color: #8F8F8F;
-    transform: rotate(45deg);
-    position: absolute;
-    top: 8px;
-    right: 35px;
-  }
-
-  .remove_action {
-    display: block;
-  }
-
-  .apartment:hover {
-    cursor: pointer;
-  }
-
-  .apartment__modal {
-    display: none;
-    position: absolute;
-    top: 55px;
-    left: 0;
-    box-shadow: 0 2px 10px 0 rgba(40, 40, 40, 0.18);
-    border-radius: 10px;
-  }
-
-  .active {
-    display: block;
-  }
-
-  .apartment__modal-point {
-    padding: 10px 20px;
-    width: 296px;
-  }
-
-  .apartment__modal-point:hover {
-    background-color: #f5f5f5;
-  }
+  
 </style>
---
+++ src/components/RealEstateTypes.vue
@@ -0,0 +1,139 @@
+<template>
+  <div>
+    <button 
+        class="apartment" 
+        @click="viemModalApartment"
+        >
+        {{ choiseApartment.length > 0 ? choiseApartment.join(', ') : 'Тип недвижимости'}}
+        <span 
+          @click.stop="removeChoisApartment"
+          class="remove"
+          :class="{remove_action: choiseApartment.length > 0}"
+        >
+        +
+        </span>
+        <span 
+          class="check"
+          :class="{check_action: modalApartment}"
+            >
+         </span>
+      </button>
+        <div  class="apartment__modal" :class="{active: modalApartment}">
+          <p 
+            @click="checkApartment"
+            class="apartment__modal-point"
+            v-for="typeApartmen in typeApartmens"
+            >
+            {{ typeApartmen }}
+          </p>
+        </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    props: {
+      typeApartmens: {
+        type: Array,
+        default: true
+      }
+    },
+    data() {
+      return {
+        modalApartment: false,
+        choiseApartment: [],
+      }
+    },
+    methods: {
+    viemModalApartment() {
+      this.modalApartment = !this.modalApartment;
+    },
+    checkApartment(e) {
+      const apartment = e.target.outerText;
+      if (!this.choiseApartment.find((i) => i === apartment)) {
+        this.choiseApartment.push(e.target.outerText)
+      } else {
+        this.choiseApartment = this.choiseApartment.filter((i) => i !== apartment)
+      }
+      this.viemModalApartment()
+    },
+    removeChoisApartment() {
+      this.choiseApartment = [];
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .apartment {
+    display: block;
+    width: 296px;
+    height: 40px;
+    padding: 12px 10px;
+    border: 1px solid #d2d2d2;
+    border-radius: 8px;
+    background-color: #fff;
+    text-rendering: optimizeSpeed;
+    display: flex;
+    align-items: center;
+    position: relative;
+  }
+
+  .check {
+    display: block;
+    width: 8px;
+    height: 8px;
+    border-bottom: 1px solid #69B574;
+    border-left: 1px solid #69B574;
+    transform: rotate(-45deg);
+    position: absolute;
+    right: 10px;
+    top: 13px;
+    transition: all .5s ease;
+  }
+
+  .check_action {
+    transform: rotate(-225deg);
+    top: 15px
+  }
+
+  .remove {
+    display: none;
+    color: #8F8F8F;
+    transform: rotate(45deg);
+    position: absolute;
+    top: 8px;
+    right: 35px;
+  }
+
+  .remove_action {
+    display: block;
+  }
+
+  .apartment:hover {
+    cursor: pointer;
+  }
+
+  .apartment__modal {
+    display: none;
+    position: absolute;
+    top: 55px;
+    left: 0;
+    box-shadow: 0 2px 10px 0 rgba(40, 40, 40, 0.18);
+    border-radius: 10px;
+  }
+
+  .active {
+    display: block;
+  }
+
+  .apartment__modal-point {
+    padding: 10px 20px;
+    width: 296px;
+  }
+
+  .apartment__modal-point:hover {
+    background-color: #f5f5f5;
+  }
+</style>
